bc0e904b61cd7a72733a9d2952851cf7156da2c0
Bianca Romero Guzman <bianca.09012001@gmail.com>
2021-11-22 16:17:48 -0400

    plantilla base agregada en views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,9 @@
-<!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-  <title>Home</title>
+@extends('master')
 
-</head>
+@section('title', 'Home')
 
+@section('content')
 <body>
-
   <!--Sidebar-->
   <div class="container-fluid">
     <div class="row">
@@ -134,10 +130,5 @@
       </div>
     </div>
   </div>
-  <!--Bootstrap-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
 </body>
-
-</html>
+@endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,10 @@
-<!doctype html>
-<html lang="en">
+@extends('master')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'Login')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-    <title>Login</title>
-</head>
+@section('content')
 
 <body class="bg-light">
-
     <!--NavBar-->
     <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <a class="navbar-brand mx-5" href="#">
@@ -52,11 +42,6 @@
             </div>
         </div>
     </section>
+</body>    
+@endsection
 
-    <!--Bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
