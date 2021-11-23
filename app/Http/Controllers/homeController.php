0fedd3963f200d04903e98d8a0d42923ffbae32b
Edson Alain Sacaca Flores <edson3103a@gmail.com>
2021-11-23 13:47:55 -0400

    Pagina de registros creada y llave primaria agregada a Desarrolla

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreRegistro;
 use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
     public function homePage(){
         return view('home');
-     }
+    }
+
+    public function registroPage() {
+        return view('registro');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\homeController;
+use App\Http\Controllers\registroController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', loginController::class)->name('login');
 
+//ruta por demas, la de arriba llama al login como ruta principal
 Route::get('/login', function () {
     return view('login');
 });
+//---------------------------------------------------------------
 
 Route::get('/home', [HomeController::class, 'homePage']);
+
+Route::get('/registro', [HomeController::class, 'registroPage']);
+
