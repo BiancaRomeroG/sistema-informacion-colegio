209f3102a6f2897694b46f3687407c2b3d03255a
GaboAP <almendrasgabriel11@gmail.com>
2021-12-01 12:15:48 -0400

    Creacion de carpetas de organizacion

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,3 @@ Route::resource('/materia', materiaController::class);
 
 Route::get('/alumno', [HomeController::class, 'alumnoPage']);
 
-
-Route::get('/crear_curso', [HomeController::class, 'curso_create']);
-
