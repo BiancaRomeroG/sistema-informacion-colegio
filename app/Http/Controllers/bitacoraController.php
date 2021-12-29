61899bc6f4e0f5a79c88145593e6e376b3743232
JhoelFloresS <jhoel1101@gmail.com>
2021-12-29 17:49:01 -0400

    bitacora index general agregada

--- app/Http/Controllers/bitacoraController.php
+++ app/Http/Controllers/bitacoraController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\acciones;
+use App\Models\administrativos;
+use App\Models\Persona;
+use App\Models\profesores;
+use App\Models\usuario;
 use DateTime;
 use Illuminate\Http\Request;
 use PDF;
@@ -10,6 +14,17 @@ use PDF;
 class bitacoraController extends Controller
 {
     
+    public function index(){
+        $acciones = acciones::orderBy('id','desc')->paginate(14);
+        $personas = [];
+        foreach ($acciones as $accion) {
+           $personas[] = usuarioController::getPersonByIdUser($accion->id_usuario); 
+        }      
+
+       // return [0 => $acciones, 1 => $personas];
+
+        return view('bitacora.index', compact('acciones' ,'personas'))->with('i', (request()->input('page', 1) - 1) * 14);
+    }
     public function show($id){
         $persona = usuarioController::getPersonByIdUser($id);
         $acciones = acciones::where('acciones.id_usuario', $id)->orderBy('id','asc')->paginate(10);
